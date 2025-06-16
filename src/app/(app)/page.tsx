df49bf9478c9241c2bfedb271d7ba8cd56cc4c17
souorichard <rodrigues.lpta@gmail.com>
2025-06-16 13:38:50 -0300

    chore: add hero section

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,3 +1,30 @@
+import { Button } from '@/components/ui/button'
+import { ArrowRight } from 'lucide-react'
+
 export default function Home() {
-  return <h1>Hello World</h1>
+  return (
+    <main>
+      {/* Hero */}
+      <section className="relative h-[500px] p-10 flex flex-col justify-end rounded-lg overflow-hidden border">
+        <div className="absolute inset-0 bg-[url('/hero-bg.jpg')] bg-cover bg-center" />
+        <div className="absolute inset-0 bg-gradient-to-t from-black to-black/40" />
+
+        <div className="relative z-10 space-y-5">
+          <h1 className="text-3xl font-bold tracking-wide">
+            I'm Richard Rodrigues
+          </h1>
+
+          <p className="max-w-2xl font-light text-pretty">
+            I'm a software engineer with a passion for building web applications
+            that are both functional and aesthetically pleasing.
+          </p>
+
+          <Button className="w-fit gap-2">
+            See more about me
+            <ArrowRight className="size-4" />
+          </Button>
+        </div>
+      </section>
+    </main>
+  )
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,7 @@ import { Code } from 'lucide-react'
 import { ThemeSwitcher } from './theme-switcher'
 import { Button } from './ui/button'
 import { NavLink } from './nav-link'
+import { Separator } from './ui/separator'
 
 const navLinks = [
   {
@@ -21,24 +22,32 @@ const navLinks = [
 export function Header() {
   return (
     <header className="flex items-center justify-between">
-      <div className="w-10 h-7 rounded-full bg-gradient-to-br from-sky-500 to-violet-500 flex items-center justify-center">
-        <Code className="size-4" />
+      <div className="flex items-center gap-2">
+        <div className="size-7 rounded-full bg-gradient-to-br from-sky-500 to-violet-500 flex items-center justify-center">
+          <Code className="size-4" />
+        </div>
+        <span className="font-medium">Hi, there!</span>
       </div>
 
-      <nav className="flex items-center gap-4">
-        {navLinks.map((link) => (
-          <Button
-            key={link.label}
-            variant="ghost"
-            className="data-[current=true]:text-sky-500 data-[current=true]:font-semibold data-[current=false]:font-normal"
-            asChild
-          >
-            <NavLink href={link.href}>{link.label}</NavLink>
-          </Button>
-        ))}
-      </nav>
+      <div className="flex items-center gap-4">
+        <nav className="flex items-center gap-4">
+          {navLinks.map((link) => (
+            <Button
+              key={link.label}
+              size="sm"
+              variant="ghost"
+              className="data-[current=true]:text-sky-500 data-[current=true]:font-semibold data-[current=false]:font-normal"
+              asChild
+            >
+              <NavLink href={link.href}>{link.label}</NavLink>
+            </Button>
+          ))}
+        </nav>
 
-      <ThemeSwitcher />
+        <Separator orientation="vertical" className="!h-4" />
+
+        <ThemeSwitcher />
+      </div>
     </header>
   )
 }
