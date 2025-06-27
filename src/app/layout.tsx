cdaa6383302b8a0f8f87f807998419d8fbfa7ec2
souorichard <rodrigues.lpta@gmail.com>
2025-06-27 17:17:25 -0300

    style: add project theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono as GeistMono } from 'next/font/google'
+import { Rubik } from 'next/font/google'
 import './globals.css'
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = GeistMono({
-  variable: '--font-geist-mono',
+const rubik = Rubik({
+  variable: '--font-rubik-sans',
   subsets: ['latin'],
 })
 
@@ -24,11 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${rubik.variable} dark antialiased`}>{children}</body>
     </html>
   )
 }
